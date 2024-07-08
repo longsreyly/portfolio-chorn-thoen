82024975bc2dfa2b9061509fac7ab68054a1a6de
longsreyly <longsreyly31@gmail.com>
2024-07-08 09:14:34 +0700

    fix link

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -28,7 +28,7 @@ const ContactPage = () => {
                                 </div>
                             </div>
                             {/* button send message */}
-                            <a href="mailto:longsreyly85@gmail.com" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-primary-foreground hover:bg-gray-800 h-10 px-4 py-2" type="submit">
+                            <a href="mailto:thoenchorn@gmail.com" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-primary-foreground hover:bg-gray-800 h-10 px-4 py-2" type="submit">
                                 Send Message
                             </a>
                         </form>
